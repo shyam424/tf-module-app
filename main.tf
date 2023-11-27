26d855eb3b3a90d9fb31848b2aeaa80bca079e38
Shyam <shyamneo24@gmail.com>
2023-11-27 11:20:46 +0530

    vpc

--- main.tf
+++ main.tf
@@ -158,3 +158,60 @@ resource "aws_lb_listener_rule" "public" {
     }
   }
 }
+
+#Instance policies->
+resource "aws_iam_policy" "main" {
+  name        = "{local.name_prefix}-policy"
+  path        = "/"
+  description = "{local.name_prefix}-policy"
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "VisualEditor0",
+        "Effect": "Allow",
+        "Action": [
+          "ssm:GetParameterHistory",
+          "ssm:GetParametersByPath",
+          "ssm:GetParameters",
+          "ssm:GetParameter"
+        ],
+        "Resource": "arn:aws:ssm:us-east-1:997483553536:parameter/docdb.${var.env}.*"
+      },
+      {
+        "Sid": "VisualEditor1",
+        "Effect": "Allow",
+        "Action": "ssm:DescribeParameters",
+        "Resource": "*"
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role" "main" {
+  name = "{local.name_prefix}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = merge(local.tags, {Name = "${local.name_prefix}-ec2"})
+}
+
+
+resource "aws_iam_role_policy_attachment" "attach" {
+  role       = aws_iam_role.main.name
+  policy_arn = aws_iam_policy.main.arn
+}
